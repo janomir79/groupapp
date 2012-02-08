af97a774f4da292a6302bc2e510ab9a01dbeec47
Oto Brglez <otobrglez@gmail.com>
2012-02-08 12:33:09 +0100

    Update...

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source "http://rubygems.org"
 gem 'sinatra'
 gem 'sass'
 gem 'haml'
+gem 'omniauth'
+gem 'omniauth-facebook'
 
 group :test, :development do
 	gem "rack-test"
--- lib/groupapp.rb
+++ lib/groupapp.rb
@@ -1,17 +1,35 @@
 # By Oto Brglez - <oto.brglez@opalab.com> 
 
 require 'bundler'
+require 'json'
 Bundler.require
+
+# OmniAuth.config.full_host = "http://localhost:9393/" #"http://groupapp.dev:9393/"
 
 class Groupapp < Sinatra::Base
 
-	set :public, 'public'
+	use Rack::Session::Cookie
+	use OmniAuth::Builder do
+		provider :facebook, ENV['GROUPAPP_FB_KEY'], ENV['GROUPAPP_FB_SECRET']
+	end
+
 	set :views, 'views'
 
 	get '/main.css' do scss :main; end
 
 	get '/' do
 		haml :landing_page
+	end
+
+	post '/auth/:name/callback' do
+		auth = request.env['omniauth.auth']
+
+		auth.to_json
+	end
+
+	get '/testout' do
+		me = {name: "Oto", priimek: "Brglez"}
+		me.to_json
 	end
 
 end
